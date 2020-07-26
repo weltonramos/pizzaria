94b5ca0bf5927ac80bd8fc32bc8077c46f6b3a94
Welton Ramos <wel035@hotmail.com>
2020-07-25 21:13:55 -0300

    Finalização do fluxo de pedido + refactoring com Sonar Lint

--- src/main/java/pizza/price/calculator/App.java
+++ src/main/java/pizza/price/calculator/App.java
@@ -1,44 +1,72 @@
 package pizza.price.calculator;
 
+import pizza.price.calculator.enums.Extra;
+import pizza.price.calculator.enums.Flavor;
 import pizza.price.calculator.enums.Pasta;
 import pizza.price.calculator.model.Pizza;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+    private static final String OPCAO_INVALIDA = "Opção inválida!";
 
     public static void main(String[] args) {
-//        System.out.println(new App().getGreeting());
+        try{
+            System.out.println("Bem-vindo a Caedson Pizzaria!");
+            System.out.println("Escolha o tipo da massa da sua pizza:\n");
 
-        System.out.println("Bem-vindo a Caedson Pizzaria!");
-        System.out.println("Escolha o tipo da massa da sua pizza:\n");
+            Scanner scanner = new Scanner(System.in);
 
-        pastaMenu();
+            pastaMenu();
 
-        Scanner scanner = new Scanner(System.in);
-        int pasta = scanner.nextInt();
+            Pasta pasta = selectPasta(scanner);
 
-        while (pasta < 1 || pasta > 5) {
-            System.out.println("Opção inválida");
-            pasta = scanner.nextInt();
+            System.out.println("Escolha o recheio de sua pizza:");
+            List<Flavor> flavorList = new ArrayList<>();
+            flavorList.add(selectFlavor(scanner));
+
+            System.out.println("Deseja acrescentar algum recheio extra? (Y/N)");
+            extraFlavor(scanner, flavorList);
+
+            System.out.println("Deseja acrescentar algum ingrediente extra? (Y/N)");
+            String extra = scanner.next();
+
+            Set<Extra> extraSet = new HashSet<>();
+
+            while (!extra.equalsIgnoreCase("n")) {
+                if (extra.equalsIgnoreCase("y")) {
+                    selectExtra(scanner, extraSet);
+                } else {
+                    System.out.println(OPCAO_INVALIDA);
+                }
+                System.out.println("Deseja algum ingrediente extra? (Y/N)");
+                extra = scanner.next();
+            }
+
+            Pizza pizza = new Pizza();
+
+            pizza.setPasta(pasta);
+            pizza.setPrice(Pasta.toEnum(pasta.getOption()).getPrice());
+            pizza.setFlavors(flavorList);
+            pizza.setExtras(extraSet);
+
+            if (flavorList.size() > 1) {
+                pizza.setPrice(flavorList.get(0).getPrice());
+                for (int i = 1; i < flavorList.size(); i++) {
+                    pizza.setPrice(flavorList.get(i).getPrice() * 0.75);
+                }
+            } else {
+                pizza.setPrice(flavorList.get(0).getPrice());
+            }
+
+            if (!extraSet.isEmpty()) {
+                extraSet.forEach(ingredient -> pizza.setPrice(ingredient.getPrice() * 0.95));
+            }
+            System.out.println(pizza.toString());
+
+        } catch (Exception e){
+            e.printStackTrace();
         }
-
-        Pizza pizza = new Pizza();
-        pizza.setPasta(Pasta.toEnum(pasta));
-        pizza.setPrice(Pasta.toEnum(pasta).getPrice());
-
-        System.out.println("Massa: " + pizza.getPasta().getDescription() +
-                " - R$" + pizza.getPrice());
-
-        System.out.println("Escolha o sabor de sua pizza:");
-
-        selectFlavor(scanner);
-
-        System.out.println("Deseja acrescentar algum recheio extra?");
-        String option = scanner.next();
     }
 
     private static void pastaMenu() {
@@ -49,19 +77,58 @@ public class App {
         System.out.println("1 - Muçarela\n2 - Presunto\n3 - Calabresa\n4 - Frango\n5 - Atum");
     }
 
+    private static Pasta selectPasta(Scanner scanner) {
+
+        int pasta = scanner.nextInt();
+
+        while (pasta < 1 || pasta > 5) {
+            System.out.println(OPCAO_INVALIDA);
+            pasta = scanner.nextInt();
+        }
+        return Pasta.toEnum(pasta);
+    }
+
+    private static Flavor selectFlavor(Scanner scanner) {
+        flavorMenu();
+        int flavor = scanner.nextInt();
+
+        while (flavor < 1 || flavor > 5) {
+            System.out.println(OPCAO_INVALIDA);
+            flavor = scanner.nextInt();
+        }
+        System.out.println("Recheio: " + Flavor.toEnum(flavor) + " R$ " + Flavor.toEnum(flavor).getPrice());
+        return Flavor.toEnum(flavor);
+    }
+
+    private static void extraFlavor(Scanner scanner, List<Flavor> flavorList) {
+        String option = scanner.next();
+
+        while (!option.equalsIgnoreCase("n")) {
+            if (option.equalsIgnoreCase("y")) {
+                flavorList.add(selectFlavor(scanner));
+            } else {
+                System.out.println(OPCAO_INVALIDA);
+            }
+            System.out.println("Mais um recheio extra? (Y/N)");
+            option = scanner.next();
+        }
+    }
+
     private static void extraMenu() {
         System.out.println("1 - Bacon\n2 - Molho\n3 - Ervilha\n4 - Cebola\n5 - Alho\n6 - Tomate\n" +
                 "7 - Oregano\n8 - Sal\n9 - Molho de tomate");
     }
 
-    private static void selectFlavor(Scanner scanner){
-        flavorMenu();
+    private static Set<Extra> selectExtra(Scanner scanner, Set<Extra> extraSet) {
+        extraMenu();
+        int extra = scanner.nextInt();
 
-        int flavor = scanner.nextInt();
-
-        while (flavor < 1 || flavor > 5) {
-            System.out.println("Opção inválida");
-            flavor = scanner.nextInt();
+        while (extra < 0 || extra > 5) {
+            System.out.println(OPCAO_INVALIDA);
+            extra = scanner.nextInt();
         }
+        extraSet.add(Extra.toEnum(extra));
+
+        return extraSet;
     }
 }
--- src/main/java/pizza/price/calculator/model/Pizza.java
+++ src/main/java/pizza/price/calculator/model/Pizza.java
@@ -4,13 +4,14 @@ import pizza.price.calculator.enums.Extra;
 import pizza.price.calculator.enums.Flavor;
 import pizza.price.calculator.enums.Pasta;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Pizza {
 
     private Pasta pasta;
-    private Flavor[] flavors;
-    private Extra[] extras;
+    private List<Flavor> flavors;
+    private Set<Extra> extras;
     private double price = 0.00;
 
     public Pasta getPasta() {
@@ -21,19 +22,19 @@ public class Pizza {
         this.pasta = pasta;
     }
 
-    public Flavor[] getFlavors() {
+    public List<Flavor> getFlavors() {
         return flavors;
     }
 
-    public void setFlavors(Flavor[] flavors) {
+    public void setFlavors(List<Flavor> flavors) {
         this.flavors = flavors;
     }
 
-    public Extra[] getExtras() {
+    public Set<Extra> getExtras() {
         return extras;
     }
 
-    public void setExtras(Extra[] extras) {
+    public void setExtras(Set<Extra> extras) {
         this.extras = extras;
     }
 
@@ -47,10 +48,9 @@ public class Pizza {
 
     @Override
     public String toString() {
-        return "Sua pizza" +
-                "Massa:" + pasta +
-                "Recheio: " + Arrays.toString(flavors) +
-                "Extras: " + Arrays.toString(extras) +
-                "Valor: " + price;
+        return "Massa : " + pasta +
+                "\nRecheio(s) : " + flavors +
+                "\nIngredientes adicionais : " + extras +
+                "\nValor : " + price;
     }
 }
